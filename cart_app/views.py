8099c65c1b7a8695a93fa7f444049f182a174c97
ycv005 <ycverma005@gmail.com>
2019-10-26 00:06:14 +0530

    cart creation improvement

--- cart_app/views.py
+++ cart_app/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-
+from .models import Cart
 # Create your views here.
+
+def cart_create():
+    cart = Cart.object.create(user=None)
+    return cart
 
 def cartHome(request):
     # following is the way to check all the variables present in the req session
     # for key, value in request.session.items():
     #     print('{} => {}'.format(key, value))
-    cart_id = request.session.get("cart_id",None) #
-    if cart_id is None:
-        request.session["cart_id"] = 121
+    cart_id = request.session.get("cart_id",None)
+    qs = Cart.object.filter(id=cart_id) 
+    if qs.count()==1: #if cart_id exist in database then good
+        cart = qs.first()
+    else:   #cart_id doesn't matches
+        cart =cart_create()
+        request.session["cart_id"] = cart.id
+
     return render(request,"cart/home.html",context={})
