6bfb9f45b9b85deaf6146fa45ba971970aa6bca4
ycv005 <ycverma005@gmail.com>
2019-10-25 16:00:30 +0530

    added cart model

--- cart_app/views.py
+++ cart_app/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def cartHome(request):
-
+    # following is the way to check all the variables present in the req session
+    # for key, value in request.session.items():
+    #     print('{} => {}'.format(key, value))
+    cart_id = request.session.get("cart_id",None) #
+    if cart_id is None:
+        request.session["cart_id"] = 121
     return render(request,"cart/home.html",context={})
