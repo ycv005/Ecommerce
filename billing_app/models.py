43c3d68aa0e11e73e6ff817081fa7da40f355108
ycv005 <ycverma005@gmail.com>
2019-11-21 22:08:04 +0530

    extended manager of billing_profile, Cart

--- billing_app/models.py
+++ billing_app/models.py
@@ -2,8 +2,23 @@ from django.db import models
 # Create your models here.
 from django.conf import settings
 from django.db.models.signals import post_save
+from accounts_app.models import GuestModel
 
 User = settings.AUTH_USER_MODEL
+
+class BillingProfileManager(models.Manager):
+    def new_or_get(self, request, cart_obj):
+        user = request.user
+        created = False
+        guest_email_id = request.session.get("guest_email_id")
+        if user.is_authenticated and user.email:
+            obj, created = self.model.objects.get_or_create(user=user,email=user.email)
+        elif guest_email_id is not None:
+            guest_email_obj = GuestModel.objects.get(id=guest_email_id)
+            obj, created = self.model.objects.get_or_create(email=guest_email_obj.email)
+        else:
+            obj = None
+        return obj, created
 
 class BillingProfile(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
@@ -11,6 +26,8 @@ class BillingProfile(models.Model):
     active = models.BooleanField(default=True)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = BillingProfileManager()
 
     def __str__(self):
         return self.email
