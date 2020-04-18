6d037472849ac5f4ff31ed4246fd80b5696a0720
Yash Chandra Verma <ycverma005@gmail.com>
2020-04-18 20:27:17 +0530

    whitenoise add in middleware

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -56,13 +56,13 @@ AUTH_USER_MODEL = "accounts_app.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
