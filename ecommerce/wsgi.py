50979bb8ebac4d3849c85462c5a4494e2ad89aed
Yash Chandra Verma <ycverma005@gmail.com>
2020-04-18 21:30:37 +0530

    whitenoise fix

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = WhiteNoise(application)
